580ce92a1b65f742747989b73303ecb926a4f256
Minh120692 <sunflora92612@gmail.com>
2026-07-13 16:38:56 +0700

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,4 @@
 import type { Metadata } from "next";
-import Script from "next/script";
-import "../style.css";
 
 export const metadata: Metadata = {
   title: "EC Support Match | Get Optimal Call Center Solution Within 1 Business Day",
@@ -15,26 +13,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ja">
-      <body>
-        <Script
-          id="gtm-script"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-T4NNLRW');`,
-          }}
-        />
-        <noscript>
-          <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-T4NNLRW"
-            height="0"
-            width="0"
-            style={{ display: "none", visibility: "hidden" }}
-          />
-        </noscript>
+      <body style={{ margin: 0 }}>
         {children}
       </body>
     </html>
